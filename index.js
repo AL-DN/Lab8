0c33ce8b0cd70eaa4d8381d90f33468170c6c953
Alden Sahi <106035261+AL-DN@users.noreply.github.com>
2023-05-05 22:26:15 -0400

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-// connects the pieces together
-import { StrictMode } from 'react';                 // react
-import { createRoot } from 'react-dom/client';      // react library
-import './styles.css';                              // styles for components
-
-import App from './App';                            // code from app.js
